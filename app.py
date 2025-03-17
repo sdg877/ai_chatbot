f03abcac30c2b2aa55ff294a1c3c9d0e8fa985c6
Sdg877 <sdrakegill@gmail.com>
2025-03-17 06:25:28 +0000

    Removing comments

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Serves the frontend
+    return render_template('index.html')  
 
 @app.route('/chat', methods=['POST'])
 def chat():
